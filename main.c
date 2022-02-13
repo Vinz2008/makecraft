c94b44b877b4fe82e459d5ddbe023bedc74432cc
Vinz2008 <clubfunreading@gmail.com>
2022-02-13 14:00:23 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,157 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic 3d example
+*
+*   Welcome to raylib!
+*
+*   To compile example, just press F5.
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2013-2022 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#define PLAYER_HEIGHT 5.0f
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+//----------------------------------------------------------------------------------
+// Local Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+Camera camera = { 0 };
+Vector3 cubePosition = { 0.0f, 1.5f, 0.0f };
+Vector3 PlayerPosition = {0};
+Vector3 PlayerPositionFloor = { 0 };
+BoundingBox PlayerHitBox = {0};
+BoundingBox GroundHitBox = {0};
+
+//----------------------------------------------------------------------------------
+// Local Functions Declaration
+//----------------------------------------------------------------------------------
+//static void UpdateDrawFrame(void);          // Update and draw one frame
+
+//----------------------------------------------------------------------------------
+// Main entry point
+//----------------------------------------------------------------------------------
+int main() 
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1200;
+    const int screenHeight = 675;
+    int i;
+    int i2;
+    int i3;
+
+    InitWindow(screenWidth, screenHeight, "raylib");
+    
+    camera.position = (Vector3){ 10.0f, PLAYER_HEIGHT + 2.0f, 8.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+    PlayerPosition = (Vector3){camera.position.x , camera.position.y, camera.position.z};
+    PlayerPositionFloor = (Vector3){camera.position.x , camera.position.y - PLAYER_HEIGHT, camera.position.z};
+    PlayerHitBox = (BoundingBox){PlayerPositionFloor, PlayerPosition, PlayerHitBox};
+    GroundHitBox = (BoundingBox){(Vector3){ 0.0f, 0.0f, 0.0f }, (Vector3){ 0.0f + 32.0f, 0.0f + 32.0f, 0.0f } };
+    Image DirtTextureMap = LoadImage("textures/dirt.png");
+    Texture2D DirtTexture = LoadTextureFromImage(DirtTextureMap);
+    
+    SetCameraMode(camera, CAMERA_FIRST_PERSON);
+
+    //--------------------------------------------------------------------------------------
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        UpdateCamera(&camera);
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+            DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY);
+            for (i2 = 0; i2 < 36; i2 = i2 + 2) {
+            for (i = 0; i < 36; i = i + 2) {
+            DrawCube((Vector3){cubePosition.x + (float)i, cubePosition.y, cubePosition.z + (float)i2}, 2.0f, 2.0f, 2.0f, RED);
+            DrawCubeWires((Vector3){cubePosition.x + (float)i, cubePosition.y, cubePosition.z + (float)i2}, 2.0f, 2.0f, 2.0f, BLACK);
+            DrawCubeTexture(DirtTexture, (Vector3){cubePosition.x + (float)i, cubePosition.y, cubePosition.z + (float)i2},2.0f,2.0f,2.0f ,WHITE);
+            }
+            }
+            /*for (i = 3; i < 27; i = i + 3) {
+            DrawCube((Vector3){cubePosition.x + (float)i2, cubePosition.y, cubePosition.z  + (float)i}, 2.0f, 2.0f, 2.0f, RED);
+            DrawCubeWires((Vector3){cubePosition.x + (float)i2, cubePosition.y, cubePosition.z  + (float)i}, 2.0f, 2.0f, 2.0f, BLACK); 
+            }*/
+            DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        DrawFPS(10, 10);
+        
+
+    EndDrawing();
+    /*if (!CheckCollisionBoxes(PlayerHitBox, GroundHitBox)){
+        for (i = 0; i < 20; i++) {
+        camera.position = (Vector3){ camera.position.x, camera.position.y - 0.7f , camera.position.z};
+        }
+    };*/
+    PlayerPosition = (Vector3){camera.position.x , camera.position.y, camera.position.z};
+    PlayerPositionFloor = (Vector3){camera.position.x , camera.position.y - 10.0f, camera.position.z};
+    PlayerHitBox = (BoundingBox){PlayerPositionFloor, PlayerPosition, PlayerHitBox};
+    GroundHitBox = (BoundingBox){(Vector3){ 0.0f, 0.0f, 0.0f }, (Vector3){ 0.0f + 32.0f, 0.0f + 32.0f, 0.0f } };
+    }
+#endif
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();                  // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+// Update and draw game frame
+/*static void UpdateDrawFrame(void)
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    //UpdateCamera(&camera);
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+
+            DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+            DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+            DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
+
+        DrawFPS(10, 10);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}*/
